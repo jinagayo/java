31db5997bf06eef2f3faa119aced2ea717a259e6
GD <GD@DESKTOP-KA7COUV>
2025-02-18 10:59:47 +0900

    dd

---
+++ chap1/src/chap1/HelloWorld.java
@@ -0,0 +1,19 @@
+package chap1;
+/* 여러줄주석
+ * 주석 : 실행되는 영역이 아님. 자바문법과 상관 없음
+ */
+//한줄 주석
+/*
+ * 1. public class HelloWorld  => 파일의 이름과 public class의 이름이 같아야 한다.
+ * 2. 한 개의.java 파일 내에는 한 개의 public class만 존재해야 한다.
+ * 3, 대소문자를 구분함 : public, Public 다른 단어임.
+ * 4. 자바의 소스파일의 확장자는 .java임
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
